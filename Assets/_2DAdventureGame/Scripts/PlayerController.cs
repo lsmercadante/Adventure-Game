7498e8e589f48d1243570bba50c3d21944e31ce5
Lilah Mercadante <lmerc@bu.edu>
2026-06-30 15:23:02 -0400

    added health collectibles

--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 
 {
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
-
-
+    public int maxHealth = 5;
+    int currentHealth = 1;
+    public float speed = 3.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,6 +18,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        //currentHealth = maxHealth;
 
 
     }
@@ -26,14 +28,20 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         move = MoveAction.ReadValue<Vector2>();
 
-        Debug.Log(move);
+        //Debug.Log(move);
 
     }
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed* Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+
+    }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
 
     }
 }
