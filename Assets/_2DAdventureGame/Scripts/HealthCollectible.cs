f8fc09a9a63fe89e905c27304a878aa7b019ae06
Lilah Mercadante <lmerc@bu.edu>
2026-06-30 16:31:20 -0400

    Enabled Damage Zones

--- Assets/_2DAdventureGame/Scripts/HealthCollectible.cs
+++ Assets/_2DAdventureGame/Scripts/HealthCollectible.cs
@@ -5,7 +5,7 @@ public class HealthCollectible : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         PlayerController controller = other.GetComponent<PlayerController>();
-        if (controller != null)
+        if (controller != null && controller.health < controller.maxHealth)
         {
             controller.ChangeHealth(1);
             Destroy(gameObject);
--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -9,8 +9,14 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     Vector2 move;
     public int maxHealth = 5;
-    int currentHealth = 1;
+    public int health { get { return currentHealth; } }
+
+    int currentHealth;
     public float speed = 3.0f;
+    // Variables related to temporary invincibility
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float damageCooldown;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,7 +24,7 @@ public class PlayerController : MonoBehaviour
 
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
-        //currentHealth = maxHealth;
+        currentHealth = maxHealth;
 
 
     }
@@ -28,18 +34,34 @@ public class PlayerController : MonoBehaviour
     {
         move = MoveAction.ReadValue<Vector2>();
 
-        //Debug.Log(move);
+        if (isInvincible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvincible = false;
+            }
+        }
 
     }
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * speed* Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
 
     }
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
 
