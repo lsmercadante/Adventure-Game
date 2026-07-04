22c31675a17420339edfc43800405d20be9df541
Lilah Mercadante <lmerc@bu.edu>
2026-07-04 10:49:15 -0400

    Added projectile and animations

--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -17,14 +17,23 @@ public class PlayerController : MonoBehaviour
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float damageCooldown;
+    Animator animator;
+    Vector2 moveDirection = new Vector2(1, 0);
+    public GameObject projectilePrefab;
+    public InputAction LaunchAction;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
         MoveAction.Enable();
+        LaunchAction.Enable();
+
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
 
 
     }
@@ -33,6 +42,14 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            moveDirection.Set(move.x, move.y);
+            moveDirection.Normalize();
+        }
+        animator.SetFloat("Look X", moveDirection.x);
+        animator.SetFloat("Look Y", moveDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
         {
@@ -41,6 +58,10 @@ public class PlayerController : MonoBehaviour
             {
                 isInvincible = false;
             }
+        }
+        if (LaunchAction.WasPressedThisFrame())
+        {
+            Launch();
         }
 
     }
@@ -61,9 +82,18 @@ public class PlayerController : MonoBehaviour
             }
             isInvincible = true;
             damageCooldown = timeInvincible;
+            animator.SetTrigger("Hit");
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+
+    }
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(moveDirection, 300);
+        animator.SetTrigger("Launch");
 
     }
 }
---
+++ Assets/_2DAdventureGame/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        EnemyController enemy = other.GetComponent<EnemyController>();
+        if (enemy != null)
+        {
+            enemy.Fix();
+        }
+        Destroy(gameObject);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
